3adc7a4344a2db8dfd63c2880c8c9e983754eee8
fadhel1597 <fadhel1597@gmail.com>
2023-09-20 22:24:29 +0700

    rest api for keras model and scikit learn model

--- restapi.py
+++ restapi.py
@@ -1,37 +1,58 @@
-from flask import Flask, request, jsonify
+import json
 import joblib
+import numpy as np
 import pandas as pd
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import argparse
 
 app = Flask(__name__)
 
-@app.route("/hypertension-predicition", methods=["POST"])
-def hypertension_predicition():
+# Define the mean and std values for standardization (replace with your actual values)
+mean = np.array([5.37250e+01, 4.75000e-01, 7.50000e-01, 1.24575e+02, 2.44000e+02, 1.00000e-01, 6.75000e-01, 1.49150e+02, 3.00000e-30, 1.27000e+00, 1.30000e+00, 7.00000e-01, 2.25000e+00])
+std = np.array([16.34717493,  0.50573633,  1.00638984, 16.20206935, 66.04970933, 0.30382181,  0.57233216, 23.42642602,  0.46409548,  1.21891082, 0.72324057,  1.1140133 ,  0.66986413])
 
-    data = request.get_json()
-    data = pd.DataFrame(data)
+# Parse command line arguments to determine which model to use
+parser = argparse.ArgumentParser(description="Choose model type for prediction")
+parser.add_argument("--model-type", type=str, choices=["keras", "scikit-learn"], required=True)
+args = parser.parse_args()
 
-    if model.endswith('.pkl'):
+if args.model_type == "keras":
+    # Load the trained Keras model
+    keras_model = load_model("weights/ANN.h5")
 
-        file = open(model,"rb")
-        trained_model = joblib.load(file)
+@app.route("/hypertension-prediction", methods=["POST"])
+def hypertension_prediction():
+    try:
+        data = request.get_json()
+        data = pd.DataFrame([data])
 
-        y_pred_prob = trained_model.predict_proba(data.reshape(1,-1))
-        y_pred = (y_pred_prob >= 0.4).astype(int)
+        # Standardize the new data point using the same scaling parameters as used for training
+        X_pred_scaled = (data - mean) / std
 
-        for pred in y_pred:
-            if pred[0] == 1:
-                class_name = 'Normal'
-            else:
-                class_name = 'Hypertension'
-        
-        condition = {
-            'condition' : class_name
+        if args.model_type == "keras":
+            # Make predictions using the Keras model
+            y_pred_prob = keras_model.predict(X_pred_scaled)
+            threshold = 0.4
+            y_pred = (y_pred_prob[:, 0] >= threshold).astype(int)  # Adjust the column index based on your model's output
+        else:
+            # Load and use the scikit-learn based model
+            file = open("weights/SVM.pkl", "rb")
+            trained_model = joblib.load(file)
+
+            # Make predictions using the scikit-learn model
+            y_pred_prob = trained_model.predict_proba(X_pred_scaled)
+            threshold = 0.4
+            y_pred = (y_pred_prob[:, 1] >= threshold).astype(int)  # Adjust the column index based on your model's output
+
+        result = {
+            "predicted_class": int(y_pred[0]),  # Ensure that the predicted class is of type int
+            "probability_of_positive_class": float(y_pred_prob[0][0]) if args.model_type == "keras" else float(y_pred_prob[0][1])  # Ensure that the probability is of type float
         }
-    elif model.endswith('.h5'):
-        pass
 
-    return jsonify(condition)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    model = "weights/SVM.pkl"
     app.run(host="0.0.0.0", port=5001, debug=True)
